9820ac58aa627ff41f231db5f6e6a89647a8a6d2
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-11-21 21:19:02 +0000

    reject me

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 import axios from 'axios'; 
+import socketIOClient from "socket.io-client";
+//const ENDPOINT = 'http://localhost:8080/';
+
+
+
 
 axios.get('/api/players')
   .then((response) => {
     console.log(response.data.players); 
   });
 
+
 function App() {
+ 
+
+  // useEffect(() => {
+  //   const socket = socketIOClient(ENDPOINT, { transport : ['websocket'] });
+  //   socket.on("test", data => {
+  //     console.log('server said hi to me!!');
+  //   });
+  // }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>.
+          yyfffy <code>src/App.js</code>.
         </p>
         <a
           className="App-link"
