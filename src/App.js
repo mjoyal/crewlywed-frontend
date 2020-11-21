a1908386998482098fd6903679ff59b0d532fa28
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-11-21 22:07:43 +0000

    socket connecction

--- src/App.js
+++ src/App.js
@@ -2,32 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import {useEffect, useState} from 'react';
+import socketIOClient from 'socket.io-client';
+const ENDPOINT = "http://localhost:8080";
+
 
 function App() {
   const [players, setPlayers] = useState([]);
   const [loading, setLoading] = useState(true);
 
+
   useEffect(() => {
-    Promise.resolve(axios.get('/api/players'))
-    .then((response) => {
-      setPlayers(response.data.players);
-      setLoading(false);
-    })
+    const socket = socketIOClient(ENDPOINT);
+    setLoading(false);
   }, []);
+
+
+
+
+  
+  // useEffect(() => {
+  //   Promise.resolve(axios.get('/api/players'))
+  //   .then((response) => {
+  //     setPlayers(response.data.players);
+  //     setLoading(false);
+  //   })
+  // }, []);
 
   if (loading) {
     return null;
   };
 
-  console.log(players);
-  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {players[0].username}.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
