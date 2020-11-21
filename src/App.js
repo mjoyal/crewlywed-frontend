6a2db0265e73335aa37d80bebc5f54ac4bd405f1
Will <wdportman@gmail.com>
2020-11-21 20:26:06 +0000

    Get data from API to render

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios'; 
-
-axios.get('/api/players')
-  .then((response) => {
-    console.log(response.data.players); 
-  });
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    Promise.resolve(axios.get('/api/players'))
+    .then((response) => {
+      setPlayers(response.data.players);
+      setLoading(false);
+    })
+  }, []);
+
+  if (loading) {
+    return null;
+  };
+
+  console.log(players);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>.
+          {players[0].username}.
         </p>
         <a
           className="App-link"
