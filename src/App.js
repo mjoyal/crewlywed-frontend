121417526c89b71806c0aa840f3f52fa33214689
Chantal Snazel <clsnazel@gmail.com>
2020-11-22 23:04:36 -0800

    uncomment app.js socket code

--- src/App.js
+++ src/App.js
@@ -14,78 +14,78 @@ import TextArea from './components/TextArea';
 import TextInput from './components/TextInput';
 
 
-// const ENDPOINT = "http://localhost:8080";
-// const socket = io(ENDPOINT);
+const ENDPOINT = "http://localhost:8080";
+const socket = io(ENDPOINT);
 
 function App() {
   
-  // const [numRows, setNumRows] = useState("n/a")
-  // const [avatar, setAvatar] = useState("https://www.seekpng.com/png/small/115-1150053_avatar-png-transparent-png-royalty-free-default-user.png")
+  const [numRows, setNumRows] = useState("n/a")
+  const [avatar, setAvatar] = useState("https://www.seekpng.com/png/small/115-1150053_avatar-png-transparent-png-royalty-free-default-user.png")
 
 
-  // // BELOW: Example of getting data from the DB via JSON object from server, upon connection:
-  // useEffect(() => {
-  //   socket.on('getSubmissions', submissionsObject => {
-  //     console.log(submissionsObject);
-  //   });
-  //   }, []);
+  // BELOW: Example of getting data from the DB via JSON object from server, upon connection:
+  useEffect(() => {
+    socket.on('getSubmissions', submissionsObject => {
+      console.log(submissionsObject);
+    });
+    }, []);
 
 
-  // //BELOW: CHAT ROOMS TEST:
-  //   const joinRoom = function () {
-  //     const roomCode = document.querySelector('#test').value;
-  //     console.log('join room!')
-  //     socket.emit('join room', roomCode); 
-  //   };
+  //BELOW: CHAT ROOMS TEST:
+    const joinRoom = function () {
+      const roomCode = document.querySelector('#test').value;
+      console.log('join room!')
+      socket.emit('join room', roomCode); 
+    };
 
-  //   const sendMessage = function () {
-  //     const message = document.querySelector('#message-test').value;
-  //     const name = document.querySelector('#name-test').value;
-  //     const room = document.querySelector('#test').value;
-  //     const messageData = {
-  //       message, 
-  //       name, 
-  //       room
-  //     }
-  //     socket.emit('message', messageData);
-  //   };
+    const sendMessage = function () {
+      const message = document.querySelector('#message-test').value;
+      const name = document.querySelector('#name-test').value;
+      const room = document.querySelector('#test').value;
+      const messageData = {
+        message, 
+        name, 
+        room
+      }
+      socket.emit('message', messageData);
+    };
 
-  //   useEffect(() => {
-  //     socket.on('message', messageData => {
-  //       console.log('message', messageData.message);
-  //       console.log('from:', messageData.name);
-  //     });  
-  //   }, []);
-
-
-  // //BELOW: DATA FLOW TESTS:
-
-  // //0. Test basic data flow:
-  // useEffect(() => {
-  //   socket.emit('hi',{name: "Will"});
-  //   }, []);
+    useEffect(() => {
+      socket.on('message', messageData => {
+        console.log('message', messageData.message);
+        console.log('from:', messageData.name);
+      });  
+    }, []);
 
 
-  // //1. countRows:
-  // const getRowCount = function () {
-  //   const table = document.querySelector('#getRowCount').value;
-  //   socket.emit('rowCount', table)
-  // };
+  //BELOW: DATA FLOW TESTS:
 
-  // socket.on('rowCountReturn', rowCount => {
-  //   setNumRows(rowCount);
-  // });
+  //0. Test basic data flow:
+  useEffect(() => {
+    socket.emit('hi',{name: "Will"});
+    }, []);
 
 
-  // //2. countRows:
-  // const getAvatar = function () {
-  //   const userID = document.querySelector('#getAvatar').value;
-  //   socket.emit('avatar', userID)
-  // };
+  //1. countRows:
+  const getRowCount = function () {
+    const table = document.querySelector('#getRowCount').value;
+    socket.emit('rowCount', table)
+  };
 
-  // socket.on('avatarReturn', avatar => {
-  //   setAvatar(avatar);
-  // });
+  socket.on('rowCountReturn', rowCount => {
+    setNumRows(rowCount);
+  });
+
+
+  //2. countRows:
+  const getAvatar = function () {
+    const userID = document.querySelector('#getAvatar').value;
+    socket.emit('avatar', userID)
+  };
+
+  socket.on('avatarReturn', avatar => {
+    setAvatar(avatar);
+  });
 
 
   return (
@@ -95,7 +95,7 @@ function App() {
         <div className="App">
           
           {/* CHAT ROOMS TEST: */}
-          {/* <header className="App-header">
+          <header className="App-header">
             <h3> Chat rooms test</h3>
             <input id="test" type="text" placeholder="enter room code" />
             <br></br>
@@ -106,7 +106,7 @@ function App() {
             <Button confirm onClick={sendMessage}>Send</Button>
           </header>
           <br></br>
-          <br></br> */}
+          <br></br>
 
           <p> -------------------------------------------------- </p>
           
@@ -115,21 +115,21 @@ function App() {
             <h3> Data flow tests</h3>
 
             {/* 1. countRows */}
-            {/* <p>1. Get the number of rows from one of the tables in the DB:</p>
+            <p>1. Get the number of rows from one of the tables in the DB:</p>
             <input id="getRowCount" type="text" placeholder="Insert table name" />
             <Button onClick={getRowCount}>Get row count</Button>
             <p>{numRows}</p>
-            <br></br> */}
+            <br></br>
 
             {/* 2. getAvatar */}
-            {/* <p>2. Get the avatar image for a player:</p>
+            <p>2. Get the avatar image for a player:</p>
             <input id="getAvatar" type="text" placeholder="Insert player ID" />
             <Button onClick={getAvatar}>Get avatar image</Button>
             <img className="testImage"
               src={avatar}
               alt="Avatar"
             />
-            <br></br> */}
+            <br></br>
 
           </header>
           
