fb09a4d0aa7c80655e2213119ffe0a401f2cbae1
Will <wdportman@gmail.com>
2020-11-22 16:09:06 -0800

    Organize app.js & set up component for data flow tests

--- src/App.js
+++ src/App.js
@@ -9,32 +9,35 @@ const ENDPOINT = "http://localhost:8080";
 const socket = io(ENDPOINT);
 
 function App() {
-  // const [players, setPlayers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  
+  // // BELOW: This can be used in conjunction w/ useEffect to make sure the app doesn't render until data is received:
+  // const [loading, setLoading] = useState(true);
+  // // THEN: Put the following right about the return of this function:
+  // if (loading) {
+  //   return null;
+  // };
+  // // AND: Put the below inside the useEffect:
+  // setLoading(false);  
  
+
+  // BELOW: Example of getting data from the DB via JSON object from server, upon connection:
   useEffect(() => {
-    setLoading(false);  
-    socket.on('avatar', function(playerInfo) {
-      console.log(playerInfo);
+    socket.on('getSubmissions', submissionsObject => {
+      console.log(submissionsObject);
     });
     }, []);
 
-    // const createRoom = function() {
-    //   console.log('create room!')
-    //   socket.emit('create room'); 
-    // }; 
 
+  //BELOW: Chatrooms example:
     const joinRoom = function () {
       const roomCode = document.querySelector('#test').value;
       console.log('join room!')
       socket.emit('join room', roomCode); 
     }
-
     const sendMessage = function () {
       const message = document.querySelector('#message-test').value;
       const name = document.querySelector('#name-test').value;
       const room = document.querySelector('#test').value;
-
       const messageData = {
         message, 
         name, 
@@ -42,34 +45,29 @@ function App() {
       }
       socket.emit('message', messageData);
     }
-
-    // listeners 
-    socket.on('show room', (room) => {
-      console.log('here is the room name', room);
-    })
-
     socket.on('message', (messageData) => {
       console.log('message', messageData.message);
       console.log('from:', messageData.name);
     });
 
-  if (loading) {
-    return null;
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-      <input id="test" type="text" placeholder="enter room code"/>
+        <h3> Chat rooms test</h3>
+        <input id="test" type="text" placeholder="enter room code" />
+        <br></br>
+        <Button confirm onClick={joinRoom}>Join</Button>
+        <br></br>
+        <input id="name-test" type="text" placeholder="enter your name" />
+        <input id="message-test" type="text" placeholder="enter message" />
+        <Button confirm onClick={sendMessage}>Send</Button>
+      </header>
       <br></br>
-      <Button confirm onClick={joinRoom}>Join</Button>
-      {/* <Button confirm onClick={createRoom}>Create</Button> */}
       <br></br>
-
-      <input id="name-test" type="text" placeholder="enter your name"/>
-      <input id="message-test" type="text" placeholder="enter message"/>
-      <Button confirm onClick={sendMessage}>Send</Button>
-      </header> 
+      <p> -------------------------------------------------- </p>
+      <header className="App-header">
+        <h3> Data flow tests</h3>
+      </header>
     </div>
   );
 }
