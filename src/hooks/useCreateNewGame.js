0c54f80d0039d718d200da1b3f8f0535873ca44d
Will <wdportman@gmail.com>
2020-11-25 21:41:20 -0800

    Add comments to code

--- src/hooks/useCreateNewGame.js
+++ src/hooks/useCreateNewGame.js
@@ -2,8 +2,10 @@ import {useEffect} from 'react';
 
 const useCreateNewGame = (socket) => {
 
+  // The hostName is passed to this hook from the NewGamePage.js component:
   let hostName = "";
 
+  // This generates a random code for the new game:
   const generateGameCode = function() {
     let code = '';
     const options = 'abcdefghijklmnopqrstuvwxyz';
@@ -13,12 +15,14 @@ const useCreateNewGame = (socket) => {
     return code;
   };
 
+  // This generates a random avatar for the host:
   const generateAvatarID = function() {
     const options = '12345678';
     const ID = options.charAt(Math.floor(Math.random() * options.length));
     return ID;
   };
 
+  // This tells the server to create a new row for the host in the players table of the DB:
   const createNewHost = function(gameID) {
     const username = hostName;
     const createNewHostData = {     
@@ -30,6 +34,7 @@ const useCreateNewGame = (socket) => {
     socket.emit('createNewHost', createNewHostData);
   };
   
+  // This tells the server to create a new row for the new game in the sessions table of the DB:
   const createNewGame = function (name) {
     hostName = name.toLowerCase();
     const gameCode = generateGameCode();
@@ -42,13 +47,14 @@ const useCreateNewGame = (socket) => {
   };
 
   useEffect(() => {
-    socket.on('createNewGameReturn', gameID => {
+  //This listens for the server to confirm that it has created a new game:
+  socket.on('createNewGameReturn', gameID => {
       createNewHost(gameID);
       // ask server for lobby info here 
 
       socket.emit('createHostLobby', gameID);
     });
-  }, [socket]);
+  });
 
   return { createNewGame };
 };
--- src/hooks/useJoinGame.js
+++ src/hooks/useJoinGame.js
@@ -69,7 +69,7 @@ const useJoinGame = (socket) => {
     socket.on('joinGameErrorInvalid', message => {
       setErrorMessage(message);
     });
-  }, [socket]);
+  });
 
   return { joinGame, errorMessage };
 
