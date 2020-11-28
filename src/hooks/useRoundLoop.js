1511bf89f46ef3e8279d6104ecc07e20b9e76b26
Will <wdportman@gmail.com>
2020-11-27 21:21:52 -0800

    Send allRoundsData to useRoundLoop hook when host hits "start game"

--- src/hooks/useRoundLoop.js
+++ src/hooks/useRoundLoop.js
@@ -6,14 +6,23 @@ const useRoundLoop = (socket, userProfile) => {
   // This determines which roundLoop component is rendered:
   const [roundState, setRoundState] = useState('ANSWER');
 
-  // This is the unique roundID in the DB, e.g. 338:
-  const [uniqueRoundID, setUniqueRoundID] = useState();
-  
-  // This is the round number shown in the game play, e.g. 1:
-  const [currentRound, setCurrentRound] = useState(1);
+  // This is all of the rounds data for the game:
+  const [allRoundsData, setAllRoundsData] = useState();
+      // allRoundsData format:
+        // [ {id: 30, victim_id: 16, question_id: 20},
+        //   {id: 31, victim_id: 16, question_id: 8},
+        //   {id: 32, victim_id: 16, question_id: 10} ]
 
   // This is the total number of rounds that will be played during the game play, e.g. 24 (8 players * 3 rounds per player):
   const [totalRounds, setTotalRounds] = useState();
+
+  // This is the round number shown in the game play, e.g. 1:
+  const [currentRoundNum, setCurrentRoundNum] = useState(1);
+
+  // The three below lines are the roundID, victimID, and questionID for the current round:
+  const [currentRoundID, setCurrentRoundID] = useState();
+  const [currentVictimID, setCurrentVictimID] = useState();
+  const [currentQuestionID, setCurrentQuestionID] = useState();
 
   // FUNCTIONALITY
   const submitUserAnswer = function (answer) {
@@ -23,7 +32,7 @@ const useRoundLoop = (socket, userProfile) => {
       round, 
       userProfile
     };
-    socket.emit('thisUserSubmitted', (userAnswerInfo));
+    socket.emit('thisUserSubmitted', userAnswerInfo);
     setRoundState('AWAIT'); 
   };
 
@@ -34,16 +43,15 @@ const useRoundLoop = (socket, userProfile) => {
       round, 
       userProfile
     }; 
-
     // send the choice on chooseAnswer button click
     socket.emit('userChoice');
     setRoundState('AWAIT'); 
   };
 
   useEffect(() => {
+    // TRANSITIONS:
     // Listen for when to show CHOOSE (sent when timer expires for ANSWER):
     socket.on('choosePage', (choices) => {
-      console.log(choices) 
       setRoundState('CHOOSE');
     });
 
@@ -59,23 +67,28 @@ const useRoundLoop = (socket, userProfile) => {
 
     // Listen for when to show ANSWER for next round (sent when timer expires for REVEAL):
     socket.on('roundOver', () => {
-      setCurrentRound(prev => prev + 1);
+      setCurrentRoundNum(prev => prev + 1);
       setRoundState('ANSWER');
     });
 
+    // DATA FROM DATABASE:
     // Listen for total number of rounds in game (sent when the host hits "start game"):
-    socket.on('initialNumRounds', numQuestions => {
-      setTotalRounds(numQuestions);
-    })
+
+    // Listen for all rounds data for game (sent when the host hits "start game"):
+    socket.on('allRoundsData', allRoundsData => {
+      setAllRoundsData(allRoundsData);
+      setTotalRounds(allRoundsData.length);
+    });
   }, [socket]);
 
   // console.log's for testing - will delete later:
   useEffect(() => {
     console.log("totalRounds:", totalRounds);
-    console.log("currentRound:", currentRound);
-  }, [totalRounds])
+    console.log("currentRoundNum:", currentRoundNum);
+    console.log("allRoundsData:", allRoundsData)
+  }, [totalRounds, currentRoundNum, allRoundsData]);
 
-  return {roundState, uniqueRoundID, currentRound, totalRounds, submitUserAnswer, sendChoice} ;
+  return {roundState, allRoundsData, totalRounds, currentRoundNum, submitUserAnswer, sendChoice} ;
 };
 
 export { useRoundLoop };
