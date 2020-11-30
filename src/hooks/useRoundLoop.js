e360a8651a88a69bf988e6840fa6d68b035e1644
Will <wdportman@gmail.com>
2020-11-29 21:30:16 -0800

    Set state for victim version of question text

--- src/hooks/useRoundLoop.js
+++ src/hooks/useRoundLoop.js
@@ -28,6 +28,7 @@ const useRoundLoop = (socket, userProfile) => {
   const [currentVictimName, setCurrentVictimName] = useState(null);
   const [currentQuestionID, setCurrentQuestionID] = useState(null);
   const [currentQuestionText, setCurrentQuestionText] = useState('Placeholder question'); //need a string here as initialization value or else we can an error
+  const [currentQuestionTextVictim, setCurrentQuestionTextVictim] = useState('Placeholder victim question');
 
   // The below is an array of players & their status (for use on the AWAIT page):
   const [awaitState, setAwait] = useState([]);
@@ -126,6 +127,7 @@ const useRoundLoop = (socket, userProfile) => {
       setCurrentVictimAvatarID(data[0].victim_avatar_id);
       setCurrentVictimName(data[0].victim_name);
       setCurrentQuestionText(data[0].question_text);
+      setCurrentQuestionTextVictim(data[0].question_text_victim);
       setBackgroundColor(`color-${data[0].victim_avatar_id}`);
       setHighlightColor(`span-${data[0].victim_avatar_id}`)
     });
@@ -149,6 +151,7 @@ const useRoundLoop = (socket, userProfile) => {
       setCurrentVictimAvatarID(allRoundsData[currentRoundNum-1].victim_avatar_id);
       setCurrentVictimName(allRoundsData[currentRoundNum-1].victim_name);
       setCurrentQuestionText(allRoundsData[currentRoundNum-1].question_text);
+      setCurrentQuestionTextVictim(allRoundsData[currentRoundNum-1].question_text_victim);
     }
   }, [currentRoundNum]);
 
