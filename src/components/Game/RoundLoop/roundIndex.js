60406cfa19344429c60c497844c857767e7590b9
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-11-30 01:47:05 +0000

    remove test data

--- src/components/Game/RoundLoop/roundIndex.js
+++ src/components/Game/RoundLoop/roundIndex.js
@@ -17,64 +17,6 @@ const CHOOSE = "CHOOSE";
 const REVEAL = "REVEAL";
 const ROUNDSCORE = "ROUNDSCORE";
 
-const answerOptions = [
-  {
-    answer:"skateboard away",
-    id:1
-  },
-  {
-    answer:"sass the zombies away",
-    id:2
-  },
-  {
-    answer:"form a tribe full of babes",
-    id:3
-  }
-]
-
-const answerResults = [
-  {
-    playerName:"mac",
-    avatarID:1,
-    correct:true,
-    answer:"form a tribe full of babes",
-    choosers:[
-      { 
-        name: "will",
-        avatarID: 2
-      },
-      { 
-        name: "chantal",
-        avatarID: 3
-      }
-    ]
-  },
-  {
-    playerName:"will",
-    avatarID:2,
-    correct:false,
-    answer:"skateboard away",
-    choosers:[
-      { 
-        name: "will",
-        avatarID: 2
-      },
-      { 
-        name: "chantal",
-        avatarID: 3
-      }
-    ]
-  }
-]
-
-const responseTest = [
-  {id: 1, avatarID: 1, username:'mac', creator:true, answered:true},
-  {id: 2, avatarID: 2, username:'mac', creator:false, answered:false},
-  {id: 3, avatarID: 3, username:'mac', creator:false, answered:false},
-  {id: 4, avatarID: 4, username:'mac', creator:false, answered:false},
-  {id: 5, avatarID: 5, username:'mac', creator:false, answered:false}
-];
-
 export default function RoundLoop (props) {
 
   return (
