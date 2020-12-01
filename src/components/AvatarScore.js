d7c0e62eb6faee0d18d72d7a275e191c02e2fe96
MacKenzie Joyal <mackenzie.joyal@email.com>
2020-12-01 03:54:36 +0000

    updated winner decorations to have some more flair - no animations

--- src/components/AvatarScore.js
+++ src/components/AvatarScore.js
@@ -8,9 +8,9 @@ export default function AvatarScore (props) {
       {props.winner && 
         <>
         <header>
-          <img className="decoration" src="images/winnerDecorationLeft.png" alt="winner decoration left"/>
+          <img className="decoration" src="images/winnerLeftDecoration.png" alt="winner decoration left"/>
           <p><span className="winnerName">{props.name}</span> wins!</p>
-          <img className="decoration" src="images/winnerDecorationRight.png" alt="winner decoration right"/>
+          <img className="decoration" src="images/winnerRightDecoration.png" alt="winner decoration right"/>
         </header>
         <footer>
           <img src={`/images/crownAvatar${props.avatar}.png`} alt="player avatar"/>
